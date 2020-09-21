97924fa3650003fa8697d193ffc9815c316e819d
kaktus23 <nnnnn94@inbox.ru>
2020-09-21 23:55:15 +0300

    Implemented other operations

--- module-1/homework/BigInteger/biginteger.cpp
+++ module-1/homework/BigInteger/biginteger.cpp
@@ -16,26 +16,118 @@ BigInteger::BigInteger(int x) {
     }
 }
 
+BigInteger::BigInteger(std::string& s) : BigInteger() {
+    const int groupSize = 9;
+    const BigInteger multiply((int) 1e9);
+    int firstGroupSize = (int) s.size() % groupSize;
+    if (firstGroupSize > 0) {
+        std::string firstGroup = s.substr(0, firstGroupSize);
+        *this = (*this) + BigInteger(std::stoi(firstGroup));
+    }
+    int cursorPos = firstGroupSize;
+    while (cursorPos < (int) s.size()) {
+        std::string curGroup = s.substr(cursorPos, groupSize);
+        *this = (*this) * multiply;
+        *this = (*this) + BigInteger(std::stoi(curGroup));
+        cursorPos += groupSize;
+    }
+}
+
 BigInteger BigInteger::operator+(const BigInteger& b) const {
-    //TODO check signs
-    return this->add(b);
+    if (isPositive() && b.isPositive()) {
+        return this->add(b);
+    } else if (isNegative() && b.isNegative()) {
+        BigInteger aAbs = abs();
+        BigInteger bAbs = abs();
+        BigInteger result = aAbs.add(bAbs);
+        result.negative = true;
+        return result;
+    } else if (isNegative() && b.isPositive()) {
+        return b - (*this);
+    } else if (isPositive() && b.isNegative()) {
+        return (*this) - b;
+    } else {
+        assert(false);
+    }
 }
 
 BigInteger BigInteger::operator-(const BigInteger& b) const {
-    //TODO check signs;
-    return this->substract(b);
+    if (isPositive() && b.isPositive()) {
+        return this->substract(b);
+    } else if (isNegative() && b.isNegative()) {
+        BigInteger bAbs = b.abs();
+        return bAbs.substract(*this);
+    } else if (isNegative() && b.isPositive()) {
+        BigInteger aAbs = abs();
+        BigInteger result = aAbs.add(b);
+        result.negative = true;
+        return result;
+    } else if (isPositive() && b.isNegative()) {
+        BigInteger bAbs = b.abs();
+        BigInteger result = add(bAbs);
+        return result;
+    } else {
+        assert(false);
+    }
 }
 
 BigInteger BigInteger::operator*(const BigInteger& b) const {
-
+    int sign1 = sign();
+    int sign2 = b.sign();
+    int signResult = sign1 * sign2;
+    if (signResult == 0) {
+        return BigInteger(0);
+    } else if (signResult == -1) {
+        BigInteger aAbs = abs();
+        BigInteger bAbs = b.abs();
+        BigInteger result = aAbs.multiply(b);
+        result.negative = true;
+        return result;
+    } else if (signResult == 1) {
+        BigInteger aAbs = abs();
+        BigInteger bAbs = b.abs();
+        BigInteger result = aAbs.multiply(bAbs);
+        return result;
+    } else {
+        assert(false);
+    }
 }
 
 BigInteger BigInteger::operator/(const BigInteger& b) const {
+    int sign1 = sign();
+    int sign2 = b.sign();
+    int signResult = sign1 * sign2;
+    if (signResult == 0) {
+        assert(false);
+    }
 
+    BigInteger aAbs = abs();
+    BigInteger bAbs = b.abs();
+    BigInteger l = BigInteger(0);
+    BigInteger r = BigInteger(*this) + BigInteger(1);
+    while (r - l > BigInteger(1)) {
+        BigInteger m = (r + l).shr();
+        if (m * bAbs <= aAbs) {
+            l = m;
+        } else {
+            r = m;
+        }
+    }
+
+    BigInteger& result = l;
+    if (signResult == 1) {
+        result.negative = false;
+    } else if (signResult == -1) {
+        result.negative = true;
+    } else {
+        assert(false);
+    }
+    return result;
 }
 
 BigInteger BigInteger::operator%(const BigInteger& b) const {
-
+    BigInteger k = (*this) / b;
+    return (*this) - k * b;
 }
 
 BigInteger BigInteger::abs() const {
@@ -52,7 +144,7 @@ bool BigInteger::isPositive() const {
     return !this->negative;
 }
 
-bool BigInteger::wordsEq(const BigInteger &b) const {
+bool BigInteger::wordsEq(const BigInteger& b) const {
     for (int i = 0; i < std::max(words.size(), b.words.size()); i++) {
         if (getWord(i) != b.getWord(i)) {
             return false;
@@ -76,7 +168,7 @@ bool BigInteger::operator<(const BigInteger& b) const {
         return false;
     } else {
         BigInteger aAbs = abs();
-        BigInteger bAbs = abs();
+        BigInteger bAbs = b.abs();
         bool result = aAbs.words < bAbs.words;
         if (aAbs.isNegative()) {
             assert(bAbs.isNegative());
@@ -172,4 +264,124 @@ std::string BigInteger::toBinaryString() const {
         result += reversedResult[i];
     }
     return result;
+}
+
+int BigInteger::sign() const {
+    if (*this == BigInteger(0)) {
+        return 0;
+    }
+    if (negative) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
+bool BigInteger::isEven() const {
+    return (getWord(0) & 1) == 0;
+}
+
+BigInteger BigInteger::multiply(const BigInteger& b) const {
+    assert(b.isPositive());
+    if (b == BigInteger(0)) {
+        return 0;
+    }
+    if (b.isEven()) {
+        BigInteger tmp = multiply(b.shr());
+        return tmp + tmp;
+    } else {
+        BigInteger tmp = multiply(b - BigInteger(1));
+        return tmp + (*this);
+    }
+}
+
+BigInteger BigInteger::operator-() const {
+    if (*this == BigInteger(0)) {
+        return BigInteger(0);
+    } else {
+        BigInteger result(*this);
+        result.negative = !negative;
+        return result;
+    }
+}
+
+BigInteger& BigInteger::operator++() {
+    *this = (*this) + BigInteger(1);
+    return *this;
+}
+
+BigInteger BigInteger::operator++(int) {
+    BigInteger tmp = *this;
+    *this = (*this) + BigInteger(1);
+    return tmp;
+}
+
+BigInteger& BigInteger::operator--() {
+    *this = (*this) - BigInteger(1);
+    return *this;
+}
+
+BigInteger BigInteger::operator--(int) {
+    BigInteger tmp = *this;
+    *this = (*this) - BigInteger(1);
+    return tmp;
+}
+
+std::istream& operator>>(std::istream& input, BigInteger& x) {
+    std::string tmp;
+    input >> tmp;
+    x = BigInteger(tmp);
+    return input;
+}
+
+std::string BigInteger::toString() const {
+    const auto base = BigInteger(10);
+    std::string reversedResult;
+    BigInteger cur = abs();
+    while (cur > BigInteger(0)) {
+        int digit = (cur % base).getWord(0);
+        reversedResult += (char)((int)'0' + digit);
+        cur /= base;
+    }
+    std::string result;
+    if (isNegative()) {
+        result += "-";
+    }
+    for (int i = (int) reversedResult.size() - 1; i >= 0; --i) {
+        result += reversedResult[i];
+    }
+    return result;
+}
+
+std::ostream& operator<<(std::ostream& output, const BigInteger& x) {
+    output << x.toString();
+    return output;
+}
+
+BigInteger BigInteger::operator+=(const BigInteger& b) {
+    *this = (*this) + b;
+    return *this;
+}
+
+BigInteger BigInteger::operator-=(const BigInteger& b) {
+    *this = (*this) - b;
+    return *this;
+}
+
+BigInteger BigInteger::operator/=(const BigInteger& b) {
+    *this = (*this) / b;
+    return *this;
+}
+
+BigInteger BigInteger::operator*=(const BigInteger& b) {
+    *this = (*this) * b;
+    return *this;
+}
+
+BigInteger BigInteger::operator%=(const BigInteger& b) {
+    *this = (*this) % b;
+    return *this;
+}
+BigInteger::operator bool() const {
+    return (*this) != BigInteger(0);
 }
--- module-1/homework/BigInteger/biginteger.h
+++ module-1/homework/BigInteger/biginteger.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <cstdint>
 #include <string>
+#include <iostream>
 
 typedef uint32_t word;
 typedef uint64_t longWord;
@@ -9,23 +10,43 @@ class BigInteger {
 public:
     BigInteger();
     BigInteger(int x); // NOLINT(google-explicit-constructor)
+    explicit BigInteger(std::string &s);
     BigInteger operator+(const BigInteger& b) const;
     BigInteger operator-(const BigInteger& b) const;
     BigInteger operator*(const BigInteger& b) const;
     BigInteger operator/(const BigInteger& b) const;
     BigInteger operator%(const BigInteger& b) const;
 
+    BigInteger operator+=(const BigInteger&b);
+    BigInteger operator-=(const BigInteger&b);
+    BigInteger operator/=(const BigInteger&b);
+    BigInteger operator*=(const BigInteger&b);
+    BigInteger operator%=(const BigInteger&b);
 
-    bool operator==(const BigInteger &b) const;
-    bool operator!=(const BigInteger &b) const;
-    bool operator<(const BigInteger &b) const;
-    bool operator>(const BigInteger &b) const;
-    bool operator<=(const BigInteger &b) const;
-    bool operator>=(const BigInteger &b) const;
+    bool operator==(const BigInteger& b) const;
+    bool operator!=(const BigInteger& b) const;
+    bool operator<(const BigInteger& b) const;
+    bool operator>(const BigInteger& b) const;
+    bool operator<=(const BigInteger& b) const;
+    bool operator>=(const BigInteger& b) const;
+
+    BigInteger operator-() const;
+
+    BigInteger& operator++() ;
+    BigInteger operator++(int) ;
+    BigInteger& operator--() ;
+    BigInteger operator--(int) ;
 
     BigInteger abs() const;
     BigInteger shr() const;
+
+
     std::string toBinaryString() const;
+    std::string toString() const;
+    operator bool() const;
+
+    friend std::istream &operator>>(std::istream &input, BigInteger &x);
+    friend std::ostream &operator<<(std::ostream &output, const BigInteger &x);
 
 private:
     static const longWord WORD_MASK = (1ULL << 32U) - 1;
@@ -35,9 +56,11 @@ private:
 
     bool isNegative() const;
     bool isPositive() const;
-    BigInteger add(const BigInteger &b) const;
-    BigInteger substract(const BigInteger &b) const;
+    BigInteger add(const BigInteger& b) const;
+    BigInteger substract(const BigInteger& b) const;
     word getWord(int index) const;
-
-    bool wordsEq(const BigInteger &b) const;
+    bool wordsEq(const BigInteger& b) const;
+    int sign() const;
+    bool isEven() const;
+    BigInteger multiply(const BigInteger& b) const;
 };
